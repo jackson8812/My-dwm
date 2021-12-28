750efa7a094271f6faf9161a3cfe236bbd4a7359
jackson8812 <rjackson.8812@gmail.com>
2021-12-28 12:46:02 -0500

    added apt packages to install

---
+++ scripts/apt-installs.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo apt install make gcc libx11-dev libxft-dev libxinerama-dev xorg git flameshot picom bash-completion xterm diodon arandr lxappearance libnotify4 notification-daemon network-manager-gnome xautolock volumeicon-alsa kdeconnect pulseaudio
