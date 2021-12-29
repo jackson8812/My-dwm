9e9cd5888821befab876356f1b5c2074f6d10d0a
jackson8812 <rjackson.8812@gmail.com>
2021-12-28 22:14:31 -0500

    updated wallpaper

--- scripts/apt-installs.sh
+++ scripts/apt-installs.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo apt install make gcc libx11-dev libxft-dev libxinerama-dev xorg git flameshot picom bash-completion xterm diodon arandr lxappearance libnotify4 notification-daemon network-manager-gnome xautolock volumeicon-alsa kdeconnect pulseaudio
+sudo apt install make gcc libx11-dev libxft-dev libxinerama-dev xorg git flameshot picom bash-completion xterm diodon arandr lxappearance libnotify4 notification-daemon libnotify-bin network-manager-gnome xautolock volumeicon-alsa kdeconnect pulseaudio vim feh thunar lightdm lightdm-gtk-greeter-settings 
