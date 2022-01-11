241d37ff474e11a8583a48061aca7d7dbd55bce7
jackson8812 <rjackson.8812@gmail.com>
2022-01-11 14:00:45 -0500

    updated dwmblocks to match my current config

--- dwmblocks/blocks.def.h
+++ dwmblocks/blocks.def.h
@@ -1,9 +1,9 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-
-	{"", "date '+%b %d (%a) %H:%M'",					5,		0},
+	{" ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	   30,		0},
+        {"", "curl -s wttr.in/Milroy+Pennsylvania?format=1",                    18000,          5},
+	{"", "date '+%a,%b %d %H:%M'",					5,		0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
